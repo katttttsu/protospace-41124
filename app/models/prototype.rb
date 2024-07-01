1c7c4d3a856e3632c4af5f236f986b08e57edfb5
katttttsu <katttttsu21@gmail.com>
2024-07-01 19:32:41 +0900

    Update prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,7 @@ class Prototype < ApplicationRecord
 
   has_one_attached :image
 
+  validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
